9343c182ff5ba8584b47553c461e0349d58adf49
tanhong <tanhong@asiainfo.com>
2021-05-18 11:40:03 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <!-- 首页 -->
 <template>
   <div class="fe-page home-page">
-
     <div class="fe-scroll-y" style="position: relative">
       <!--      导航栏-->
       <!--      header-->
@@ -19,11 +18,7 @@
                   @mouseover="stopPlay()"
                   @mouseleave="bannerShu()"
                 >
-                  <div
-                    class="flex-column-center fe-page"
-                    @click="jump(item.url)"
-                    style="position: absolute"
-                  >
+                  <div class="flex-column-center fe-page" @click="jump(item.url)" style="position: absolute">
                     <div class="bannerText">
                       <div :id="bannerImgArrayId[index].title" class="line1">
                         {{ item.title }}
@@ -31,15 +26,9 @@
                       <div :id="bannerImgArrayId[index].describe" class="line2">
                         {{ item.describe }}
                       </div>
-
                     </div>
                   </div>
-                  <img
-                    :id="bannerImgArrayId[index].src"
-                    width="100%"
-                    :src="item.src"
-                    alt=""
-                  />
+                  <img :id="bannerImgArrayId[index].src" width="100%" :src="item.src" alt="" />
                 </li>
               </transition-group>
             </div>
@@ -57,10 +46,7 @@
                 {{ containerElement1.describe }}
               </p>
             </div>
-            <div
-              v-if="hotProductId.length"
-              class="popular-container fe-flex-center w-100"
-            >
+            <div v-if="hotProductId.length" class="popular-container fe-flex-center w-100">
               <div
                 @click="jump(item.url)"
                 class="h-100 popular-content"
@@ -91,31 +77,20 @@
                 <div class="popular-bottom flex-column-between">
                   <ul :id="hotProductId[index].tag" class="flex-column-between">
                     <li v-for="tag in item.tag" :key="tag" class="fe-flex">
-
                       <span>{{ tag }}</span>
                     </li>
                   </ul>
                   <div style="height: 54px" class="popular-price flex-column-between">
                     <p>
                       价格：
-                      <span
-                        :id="hotProductId[index]"
-                        class="fe-font-lg fe-mr-sm"
-                        style="color: #ff956f"
-                      >
+                      <span :id="hotProductId[index]" class="fe-font-lg fe-mr-sm" style="color: #ff956f">
                         ￥{{ item }}.
                         <span class="fe-font-md">00</span>
                       </span>
-                      <span :id="hotProductId[index].unit" style="color: #455a74">{{
-                          item.unit
-                        }}</span>
+                      <span :id="hotProductId[index].unit" style="color: #455a74">{{ item.unit }}</span>
                     </p>
                     <p>
-                      优惠：<span
-                      :id="hotProductId[index]"
-                      style="color: #455a74"
-                    >{{ item }}</span
-                    >
+                      优惠：<span :id="hotProductId[index]" style="color: #455a74">{{ item }}</span>
                     </p>
                   </div>
                 </div>
@@ -177,48 +152,29 @@
                   <p :id="recommendationId[active]" style="font-size: 20px">
                     {{ recommendation[active] }}
                   </p>
-                  <p
-                    :id="recommendationId[active]"
-                    class="fe-flex pre-describe"
-                  >
+                  <p :id="recommendationId[active]" class="fe-flex pre-describe">
                     {{ recommendation[active] }}
                   </p>
                 </div>
               </div>
-              <div
-                class="product-end"
-                v-for="(item, index) in recommendation[active]"
-                :key="item.title"
-              >
+              <div class="product-end" v-for="(item, index) in recommendation[active]" :key="item.title">
                 <div class="fe-page end-container flex-column-between">
                   <div class="end-container-top">
-                    <p
-                      :id="recommendationId[active][index].title"
-                      class="end-container-title"
-                    >
+                    <p :id="recommendationId[active][index].title" class="end-container-title">
                       {{ item.title }}
                     </p>
                     <!--                    <p class='end-container-describe'>{{ item.describe }}</p>-->
                   </div>
-                  <ul
-                    :id="recommendationId[active][index].tag"
-                    class="end-container-tag"
-                  >
+                  <ul :id="recommendationId[active][index].tag" class="end-container-tag">
                     <li class="fe-flex-center" v-for="tag in item.tag" :key="tag">
                       {{ tag }}
                     </li>
                   </ul>
                   <div class="end-container-group fe-flex-between">
                     <div>
-                      <span
-                        :id="recommendationId[active][index]"
-                        style="color: #ff956f"
-                      >￥{{ item }}.00 </span
-                      ><span :id="recommendationId[active][index].unit">{{
-                        item.unit
-                      }}</span>
+                      <span :id="recommendationId[active][index]" style="color: #ff956f">￥{{ item }}.00 </span
+                      ><span :id="recommendationId[active][index].unit">{{ item.unit }}</span>
                     </div>
-
                   </div>
                 </div>
               </div>
@@ -252,12 +208,7 @@
                 :id="schemeMenuId[menuActive].img"
                 width="100%"
                 height="100%"
-                style="
-                  position: absolute;
-                  z-index: 0;
-                  transition: opacity 1s linear;
-                  cursor: pointer;
-                "
+                style="position: absolute; z-index: 0; transition: opacity 1s linear; cursor: pointer"
                 @click="jump(schemeMenu[menuActive].link)"
               />
               <div class="scheme-bottom flex-column-between">
@@ -267,7 +218,7 @@
                 <p :id="schemeMenuId[menuActive].value" class="bottom-describe">
                   方案价值：
                   <span v-for="(item, index) in schemeMenu[menuActive].value" :key="item">
-                    <span class="vertical-bar" v-if="index !== 0"/>
+                    <span class="vertical-bar" v-if="index !== 0" />
                     {{ item }}
                   </span>
                 </p>
@@ -285,28 +236,15 @@
               </p>
             </div>
             <ul class="information fe-flex-between">
-              <li
-                v-for="(item, index) in information"
-                @click="jump(item.url)"
-                :key="item.title"
-              >
+              <li v-for="(item, index) in information" @click="jump(item.url)" :key="item.title">
                 <div class="information-img">
-                  <img
-                    :id="informationId[index].img"
-                    :src="item.img"
-                    alt=""
-                    width="100%"
-                    height="100%"
-                  />
+                  <img :id="informationId[index].img" :src="item.img" alt="" width="100%" height="100%" />
                 </div>
                 <div class="information-container flex-column-between">
                   <p :id="informationId[index].title" class="information-container-title">
                     {{ item.title }}
                   </p>
-                  <p
-                    :id="informationId[index]"
-                    class="information-container-describe"
-                  >
+                  <p :id="informationId[index]" class="information-container-describe">
                     {{ item }}
                   </p>
                   <p :id="informationId[index].time" class="information-container-time">
@@ -318,7 +256,7 @@
           </div>
           <div class="w-100 fe-flex-center" style="margin-top: 80px">
             <div @click="jump(moreInformation.url)" class="more-product fe-flex-center">
-              {{ moreInformation.title }}<i class="el-icon-back"/>
+              {{ moreInformation.title }}<i class="el-icon-back" />
             </div>
           </div>
         </div>
@@ -332,7 +270,6 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
@@ -349,110 +286,104 @@ export default {
       schemeMenuId: [],
       informationId: [],
       active: 0,
-      activeHover: '',
-      activeHot: '',
+      activeHover: "",
+      activeHot: "",
       menuActive: 0,
 
       containerElement1: {
-        title: '热门产品',
-        describe: '为您提供专网自服务建设的热门产品。'
+        title: "热门产品",
+        describe: "为您提供专网自服务建设的热门产品。",
       },
       containerElement2: {
-        title: '专网组合产品推荐',
-        describe: '针对不同的业务需求，为您提供合适的组合产品。'
+        title: "专网组合产品推荐",
+        describe: "针对不同的业务需求，为您提供合适的组合产品。",
       },
       containerElement3: {
-        title: '精选方案',
-        describe: '为您精选包含最专业的、15大行业的5G专网解决方案。'
+        title: "精选方案",
+        describe: "为您精选包含最专业的、15大行业的5G专网解决方案。",
       },
       containerElement4: {
-        title: '行业资讯',
-        describe: '带给您最新的深度行业资讯。'
+        title: "行业资讯",
+        describe: "带给您最新的深度行业资讯。",
       },
       containerElement1Id: {},
       containerElement2Id: {},
       containerElement3Id: {},
       containerElement4Id: {},
       moreInformation: {
-        title: '更多资讯',
-        url: ''
+        title: "更多资讯",
+        url: "",
       },
       moreProducts: {
-        title: '更多产品',
-        url: ''
-      }
-    }
+        title: "更多产品",
+        url: "",
+      },
+    };
   },
-  computed: {
-  },
+  computed: {},
   methods: {
     // 查询终端商品列表信息
-    getInfoPage() {
+    getInfoPage() {},
 
-    },
-   
     // 跳转
     jump(index) {
-      window.location.href = index
+      window.location.href = index;
     },
     // banner元素
-    
+
     bannerShu() {
-      this.timer1 = setInterval(this.autoPlay, 5000)
+      this.timer1 = setInterval(this.autoPlay, 5000);
     },
     autoPlay() {
-      this.bannerNum++
-      let a = 1
+      this.bannerNum++;
+      let a = 1;
       if (a === 0) {
-        this.bannerNum = 0
+        this.bannerNum = 0;
       }
     },
     stopPlay() {
-      clearInterval(this.timer1)
+      clearInterval(this.timer1);
     },
     // banner高度自适应
     autoHeightS() {
-      this.$refs.banner.style.height = this.$refs.banner.clientWidth * 0.3125 + 'px'
+      this.$refs.banner.style.height = this.$refs.banner.clientWidth * 0.3125 + "px";
       // console.log("宽",this.$refs.banner.clientWidth)
     },
     // Although come back to normal life,we still admire those who see the daylight in fearless years.
 
     changeSemester(index) {
-      this.active = index
+      this.active = index;
     },
     changeMenu(index) {
-      this.menuActive = index
+      this.menuActive = index;
     },
     recommendationOver(index) {
-      this.activeHover = index
+      this.activeHover = index;
     },
     recommendationLeave() {
-      this.activeHover = ''
+      this.activeHover = "";
     },
     hotOver(index) {
-      this.activeHot = index
+      this.activeHot = index;
     },
     hotLeave() {
-      this.activeHot = ''
+      this.activeHot = "";
     },
-
   },
   mounted() {
     this.$nextTick(function () {
       window.onresize = () => {
-        this.autoHeightS()
-      }
-      this.autoHeightS()
-      this.bannerShu()
-      this.getInfoPage()
-    })
+        this.autoHeightS();
+      };
+      this.autoHeightS();
+      this.bannerShu();
+      this.getInfoPage();
+    });
   },
   beforeDestroy() {
-    clearInterval(this.timer1)
-  }
-}
+    clearInterval(this.timer1);
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
